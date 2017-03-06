3ffdc9a2ebf061cc9d7ff16a1b2d7e8d3082e10f
jeetiss <za5rubley@gmail.com>
2017-03-06 11:41:15 +0000

    rebuild pages at 421a3bc

--- src/selectors.js
+++
@@ -1,8 +0,0 @@
-export const selectUser = state => ({user: state.user})
-export const selectMessages = store => ({ messages: store.messages })
-export const selectRooms = store => ({ rooms: store.rooms })
-export const selectSelectedRoom = store => ({ selectedRoom: store.selectedRoom })
-
-export const composeSelectors = (...selectors) => store => Object.assign(
-  ...selectors.map(selector => selector(store))
-)
